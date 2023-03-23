f36a7bab34eef7857888d062af9201507ffabb92
James Smith <8142061+madebyjames@users.noreply.github.com>
2023-03-23 14:51:51 +0000

    feat(web): listen to the hello world handler

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", handleRoot)
+
+	err := http.ListenAndServe(":3000", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("Web server closed")
+	}
+	if err != nil {
+		fmt.Printf("Error starting web server: %s\n", err)
+		os.Exit(1)
+	}
 }
 
 func handleRoot(w http.ResponseWriter, _ *http.Request) {
